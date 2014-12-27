200edb1c768febd3543afbda0129c0785a3a6147
Jason Feng <jason@ozbert.com>
2014-12-26 17:53:44 -0800

    Get rid of height markers properly.

--- elevation/elevation.js
+++ elevation/elevation.js
@@ -55,35 +55,36 @@ return document.getElementById("cumulative").checked;;
 function showPath () {
 var el = document.querySelector("#path-stuff tbody");
 var vdeltas = (function  () {
-var _ = pathStuff.map(function  (G76318) {
-return G76318[2];;
+var _ = pathStuff.map(function  (G76323) {
+return G76323[2];;
 });
 var _ = map(function  (a, b) {
 return b-a;;
 }, _, _.slice(1));
 return _;;;
 })();
-var hdeltas = map(function  (G76319, G76320) {
-var lat1 = G76319[0];
-var lng1 = G76319[1];
-var e1 = G76319[2];
-var lat2 = G76320[0];
-var lng2 = G76320[1];
-var e2 = G76320[2];
+var hdeltas = map(function  (G76324, G76325) {
+var lat1 = G76324[0];
+var lng1 = G76324[1];
+var e1 = G76324[2];
+var lat2 = G76325[0];
+var lng2 = G76325[1];
+var e2 = G76325[2];
 return earthDistance(lat1, lng1, lat2, lng2);;
 }, pathStuff, pathStuff.slice(1));
 var cums = reductions(function  (a, b) {
 return a+b;;
 }, hdeltas);
 el.innerHTML="";
-return pathStuff.forEach(function  (G76321, ix) {
-var lat = G76321[0];
-var lng = G76321[1];
-var elevation = G76321[2];
+return pathStuff.forEach(function  (G76326, ix) {
+var lat = G76326[0];
+var lng = G76326[1];
+var elevation = G76326[2];
 var n = node(["tr", ["td", ["button", "X"]], ["td", roundTo(elevation, 3)], ["td", roundTo(((cumulativeP() ? cums : hdeltas))[ix], 3)], ["td", roundTo(vdeltas[ix]/hdeltas[ix], 3)]]);
 var deleteButton = n.querySelector("button");
 deleteButton.onclick=function  () {
 pathStuff.splice(ix, 1);
+heightMarkers[ix].setMap(null);
 heightMarkers.splice(ix, 1);
 return showPath();;
 };
@@ -114,11 +115,11 @@ return deg*Math.PI/180;;
 };
 
 function greatCircleDist (lat1, lng1, lat2, lng2) {
-var G76322 = [lat1, lng1, lat2, lng2].map(toRadians);
-var lat1 = G76322[0];
-var lng1 = G76322[1];
-var lat2 = G76322[2];
-var lng2 = G76322[3];
+var G76327 = [lat1, lng1, lat2, lng2].map(toRadians);
+var lat1 = G76327[0];
+var lng1 = G76327[1];
+var lat2 = G76327[2];
+var lng2 = G76327[3];
 return Math.asin(Math.sqrt(sin2((lng2-lng1)/2)*Math.cos(lat1)*Math.cos(lat2)+sin2((lat2-lat1)/2)))*2;;;
 };
 
